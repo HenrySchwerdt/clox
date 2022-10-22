f1fc8de781f7e22664233528d9055fcd2856ae5c
heschwerdt <henry.schwerdtner@web.de>
2022-10-22 18:59:14 +0200

    added string support

--- src/vm.c
+++ src/vm.c
@@ -1,6 +1,10 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+
 #include "../include/common.h"
+#include "../include/memory.h"
+#include "../include/object.h"
 #include "../include/vm.h"
 #include "../include/compiler.h"
 #include "../include/debug.h"
@@ -56,6 +60,19 @@ static bool isFalsey(Value value) {
 
 }
 
+static void concatenate() {
+	ObjString* b = AS_STRING(pop());
+	ObjString* a = AS_STRING(pop());
+	int length = a->length + b->length;
+	char* chars = ALLOCATE(char, length + 1);
+	memcpy(chars, a->chars, a->length);
+	memcpy(chars + a->length, b->chars, b->length);
+	chars[length] = '\0';
+
+	ObjString* result = takeString(chars, length);
+	push(OBJ_VAL(result));
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
@@ -109,7 +126,20 @@ static InterpretResult run() {
 			}
 			case OP_GREATER: BINARY_OP(BOOL_VAL, >); break;
 			case OP_LESS: BINARY_OP(BOOL_VAL, <); break;
-			case OP_ADD: BINARY_OP(NUMBER_VAL, +); break;
+			case OP_ADD: {
+				if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+					concatenate();
+				} else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+					double b = AS_NUMBER(pop());
+					double a = AS_NUMBER(pop());
+					push(NUMBER_VAL(a+b));
+				} else {
+					runtimeError(
+							"Operands must be two numbers or two strings");
+					return INTERPRET_RUNTIME_ERROR;
+				}
+				break;
+			}
 			case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
             case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
             case OP_DIVIDE: BINARY_OP(NUMBER_VAL, /); break;
